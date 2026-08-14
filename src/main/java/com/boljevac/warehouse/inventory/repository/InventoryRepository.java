417fd3958ed94ff89250a01000f62391c2b69764
Dare2510 <dario.boljevac@gmail.com>
2026-08-14 15:56:45 +0200

    fixed concurrent problem at order creation.

--- src/main/java/com/boljevac/warehouse/inventory/repository/InventoryRepository.java
+++ src/main/java/com/boljevac/warehouse/inventory/repository/InventoryRepository.java
@@ -2,7 +2,9 @@ package com.boljevac.warehouse.inventory.repository;
 
 import com.boljevac.warehouse.inventory.entity.InventoryEntity;
 import com.boljevac.warehouse.product.entity.ProductEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.List;
 @Repository
 public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	List<InventoryEntity> getAllByProductEntity(ProductEntity productEntity);
 
 	boolean existsByProductEntity(ProductEntity productEntity);
